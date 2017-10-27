0b45a591f0ff171f7aa963e794e1c6e049c4b37e
MichaelChrystal <S00164997@mail.itsligo.ie>
2017-10-27 12:00:16 +0100

    https://github.com/MichaelChrystal/Week6Lab2MVC.git

---
+++ S00164997Rad2016Mvc1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="S00164997Rad2016Mvc1.MvcApplication" Language="C#" %>
---
+++ S00164997Rad2016Mvc1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(S00164997Rad2016Mvc1.Startup))]
+namespace S00164997Rad2016Mvc1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
